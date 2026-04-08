5618805ab9a6a4320fb6caa0155a5a7c3e95edb6
Tam Trang <tuo98490@temple.edu>
2026-04-08 07:20:50 -0400

    Moved Enemy class to characterBase.py and removed attack method for Main_Character class

--- characterBase.py
+++ characterBase.py
@@ -1,17 +1,31 @@
 import random as die
-from enemyList import *
-class Main_Character():
+from abc import abstractmethod, ABC
+class Main_Character(ABC):
     def __init__(self, HP: int, DEF: int, minATK: int, maxATK: int):
         self.hp = HP
         self.defense = DEF
         self.minattack = minATK
         self.maxattack= maxATK
         
-    def attack(self):
-        return die.randint(self.minattack, self.maxattack)
     
+    @abstractmethod
     def take_damage(self, enemy):
-        self.hp -= max(0, enemy.enemy_attack() - self.defense)
+        pass
+        
+class Enemy():
+    def __init__(self, HP, minATK, maxATK):
+       self.hp = HP
+       self.minATK = minATK
+       self.maxATK = maxATK
+        
+    def enemy_attack(self):
+        return die.randint(self.minATK, self.maxATK)
+    
+    def take_damage(self, damage):
+        if damage > self.hp:
+            self.hp = 0
+        else:
+            self.hp -= damage
     
     
         
